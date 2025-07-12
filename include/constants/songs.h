8a475e422a9d1e24c341ec76d4e65ab0a593eaa2
Rodolfo Ruiz-Velasco <ruizvelascorodolfo@gmail.com>
2025-07-11 22:58:01 -0500

    Add team rocket battle theme

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -356,6 +356,7 @@
 #define MUS_JESSIE_JAMES_THEME	  348 // MUS_JESSIE_JAMES_THEME
 #define MUS_ROUTE26	              349 // MUS_ROUTE26
 #define MUS_POKE_MART	          350 // MUS_POKE_MART
+#define MUS_VS_TEAM_ROCKET	      351 // MUS_VS_TEAM_ROCKET
 
 #define MUS_NONE                    0xFFFF
 
