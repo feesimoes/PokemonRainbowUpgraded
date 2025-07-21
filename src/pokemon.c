e8cf6c3586d1ad04debaa80aa2dd6370cde04d0a
Rodolfo Ruiz-Velasco <ruizvelascorodolfo@gmail.com>
2025-07-20 22:55:37 -0500

    Elite Four Battle theme added
    It's a mix of Johto and Kanto's gym battle themes. Also some minor changes in map music to keep the indigo plateau more consistent

--- src/pokemon.c
+++ src/pokemon.c
@@ -5875,7 +5875,7 @@ static u16 GetBattleBGM(void)
                 return MUS_VS_GYM_LEADER;
             }
         case TRAINER_CLASS_ELITE_FOUR:
-            return MUS_VS_GYM_LEADER;
+            return MUS_VS_ELITE_FOUR;
         case TRAINER_CLASS_BOSS:
             return MUS_VS_TEAM_ROCKET_BOSS;
         case TRAINER_CLASS_TEAM_ROCKET:
